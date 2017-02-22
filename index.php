3292f355fa3cd4a98d31d9d2dd70a6167e6d2cdb
SoltonCmith <colton.smith67@outlook.com>
2017-02-22 15:42:36 -0500

    updated code

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $statement->closeCursor(); // close the connection
             <!-- FIX THE ADD NEW BOOK BUTTON -->
             <!-- /////////////////////////// -->
 
-            <a class="btn btn-primary" href="">
+            <a class="btn btn-primary" href="book_details.php">
                 <i class="fa fa-plus"></i> Add New Book</a>
             <br>
             <table class="table table-striped table-hover table-bordered">
