4ac0f5855e563f6b3ebfe5b6cfa60af61173d83a
SoltonCmith <colton.smith67@outlook.com>
2017-02-22 17:03:24 -0500

    updated
    git commit -m updated
    git commit -m updated

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $statement->closeCursor(); // close the connection
             <!-- FIX THE ADD NEW BOOK BUTTON -->
             <!-- /////////////////////////// -->
 
-            <a class="btn btn-primary" href="book_details.php">
+            <a class="btn btn-primary" href="book_details.php?bookID=0">
                 <i class="fa fa-plus"></i> Add New Book</a>
             <br>
             <table class="table table-striped table-hover table-bordered">
